3ddcd65aca9d22c8f6ff9a9753f58db3f4fe47ef
Marius Räsener <931919+elmcrest@users.noreply.github.com>
2024-07-29 10:57:57 +0200

    reduced dependencies

--- config/settings.py
+++ config/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = env.ENVIRON["DJANGO_SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["issue11.lcl.host"]
+ALLOWED_HOSTS = ["localhost"]
 
 
 # Application definition
